a3ca3d05c3ececc19ebe3183816509b6f647cae9
Anabol1ks <grigorogannisyan.12@gmail.com>
2024-06-26 01:19:07 +0300

    Метод Шаннона Фано

--- archiver/cmd/unpack.go
+++ archiver/cmd/unpack.go
@@ -1,12 +1,77 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"archiver/lib/compression"
+	"archiver/lib/compression/vlc"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
 
 var unpackCmd = &cobra.Command{
 	Use:   "unpack",
 	Short: "Unpack file",
+	Run:   unpack,
+}
+
+const unpackedExtension = "ana"
+
+func unpack(cmd *cobra.Command, args []string) {
+	var decoder compression.Decoder
+
+	if len(args) == 0 || args[0] == "" {
+		handleErr(ErrEmptyPath)
+	}
+
+	method := cmd.Flag("method").Value.String()
+
+	switch method {
+	case "vlc":
+		decoder = vlc.New()
+	default:
+		cmd.PrintErr("unknown method")
+	}
+
+	filePath := args[0]
+
+	r, err := os.Open(filePath)
+	if err != nil {
+		handleErr(err)
+	}
+
+	defer r.Close()
+
+	data, err := io.ReadAll(r)
+	if err != nil {
+		handleErr(err)
+	}
+
+	packed := decoder.Decode(data)
+
+	err = os.WriteFile(unpackedFileName(filePath), []byte(packed), 0644)
+	if err != nil {
+		handleErr(err)
+	}
+}
+
+func unpackedFileName(path string) string {
+	// path = /path/to/file/myFile.txt
+	fileName := filepath.Base(path) //myFile.txt
+	//.txt
+	// myFile.txt - .txt = myFile
+
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName)) + "." + unpackedExtension
 }
 
 func init() {
 	rootCmd.AddCommand(unpackCmd)
+
+	unpackCmd.Flags().StringP("method", "m", "", "decompression method: vlc")
+
+	if err := packCmd.MarkFlagRequired("method"); err != nil {
+		panic(err)
+	}
 }
---
+++ archiver/lib/compression/vlc/chunks.go
@@ -0,0 +1,89 @@
+package vlc
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode/utf8"
+)
+
+type BinaryChunks []BinaryChunk
+
+type BinaryChunk string
+
+const (
+	chunkSize = 8
+)
+
+func NewBinChunks(data []byte) BinaryChunks {
+
+	res := make(BinaryChunks, 0, len(data))
+
+	for _, code := range data {
+		res = append(res, NewBinChunk(code))
+	}
+
+	return res
+}
+
+func (bcs BinaryChunks) Bytes() []byte {
+	res := make([]byte, 0, len(bcs))
+
+	for _, bc := range bcs {
+		res = append(res, bc.Byte())
+	}
+	return res
+}
+
+func (bc BinaryChunk) Byte() byte {
+	num, err := strconv.ParseUint(string(bc), 2, chunkSize)
+	if err != nil {
+		panic("can't parse binary chunk: " + err.Error())
+	}
+	return byte(num)
+}
+
+func NewBinChunk(code byte) BinaryChunk {
+	return BinaryChunk(fmt.Sprintf("%08b", code))
+}
+
+// Join joins chunks into one line and returns as string.
+func (bcs BinaryChunks) Join() string {
+	var buf strings.Builder
+
+	for _, bc := range bcs {
+		buf.WriteString(string(bc))
+	}
+	return buf.String()
+}
+
+func splitByChunks(bStr string, chunkSize int) BinaryChunks {
+	strLen := utf8.RuneCountInString(bStr)
+	chunkCount := strLen / chunkSize
+
+	//444422 -> 4444 2200
+	if strLen/chunkSize != 0 {
+		chunkCount++
+	}
+
+	res := make(BinaryChunks, 0, chunkCount)
+
+	var buf strings.Builder
+
+	for i, ch := range bStr {
+		buf.WriteString(string(ch))
+
+		if (i+1)%chunkSize == 0 {
+			res = append(res, BinaryChunk(buf.String()))
+			buf.Reset()
+		}
+	}
+
+	if buf.Len() != 0 {
+		lastChunk := buf.String()
+
+		lastChunk += strings.Repeat("0", chunkSize-len(lastChunk))
+		res = append(res, BinaryChunk(lastChunk))
+	}
+	return res
+}
