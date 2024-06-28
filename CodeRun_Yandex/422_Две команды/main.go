4bdf5c92dcdafda4f60e36360f935a74717f206f
Anabol1ks <grigorogannisyan.12@gmail.com>
2024-06-28 16:12:44 +0300

    .

--- CodeRun_Yandex/422_Две команды/main.go
+++ CodeRun_Yandex/422_Две команды/main.go
@@ -2,20 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 )
 
 func main() {
 	var A, B, N int
 	fmt.Scan(&A, &B, &N)
-	for i := 1; i <= N; i++ {
-		for j := 1; j <= N; j++ {
-			if float64(A/j) > float64(B/i) {
-				fmt.Println("Yes")
-				os.Exit(0)
-			}
-		}
+	if math.Ceil(float64(A)) > math.Ceil((float64(B) / float64(N))) {
+		fmt.Println("Yes")
+		os.Exit(0)
 	}
 	fmt.Println("No")
-	fmt.Println(9 / 2)
 }
